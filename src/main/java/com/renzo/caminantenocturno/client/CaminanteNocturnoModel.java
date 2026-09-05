b0206a13244d3cd5e75a76413500565e5f770039
remiusomg-ops <remiusomg@gmail.com>
2026-09-05 14:03:08 -0300

    Corregir proporciones del Caminante

--- src/main/java/com/renzo/caminantenocturno/client/CaminanteNocturnoModel.java
+++ src/main/java/com/renzo/caminantenocturno/client/CaminanteNocturnoModel.java
@@ -14,27 +14,36 @@ public class CaminanteNocturnoModel<T extends CaminanteNocturnoEntity> extends H
         PartDefinition root = mesh.getRoot();
 
         root.addOrReplaceChild("head",
-                CubeListBuilder.create().texOffs(0,0).addBox(-4,-8,-4,8,8,8,new CubeDeformation(0.0F)),
+                CubeListBuilder.create().texOffs(0,0)
+                        .addBox(-4,-8,-4,8,8,8),
                 PartPose.offset(0.0F,0.0F,0.0F));
+
         root.addOrReplaceChild("hat", CubeListBuilder.create(), PartPose.ZERO);
 
         root.addOrReplaceChild("body",
-                CubeListBuilder.create().texOffs(16,16).addBox(-4,0,-2,8,12,4,new CubeDeformation(-0.25F)),
-                PartPose.offsetAndRotation(0.0F,0.0F,0.0F,0.18F,0.0F,0.0F));
+                CubeListBuilder.create().texOffs(16,16)
+                        .addBox(-3.5F,0,-2,7,12,4),
+                PartPose.offsetAndRotation(0.0F,0.0F,0.0F,0.10F,0.0F,0.0F));
 
         root.addOrReplaceChild("right_arm",
-                CubeListBuilder.create().texOffs(40,16).addBox(-3,-2,-2,4,18,4,new CubeDeformation(-0.15F)),
-                PartPose.offsetAndRotation(-5.0F,2.0F,0.0F,0.18F,0.0F,0.12F));
+                CubeListBuilder.create().texOffs(40,16)
+                        .addBox(-2.5F,-2,-2,4,14,4),
+                PartPose.offsetAndRotation(-4.5F,2.0F,0.0F,0.15F,0.0F,0.08F));
+
         root.addOrReplaceChild("left_arm",
-                CubeListBuilder.create().mirror().texOffs(40,16).addBox(-1,-2,-2,4,18,4,new CubeDeformation(-0.15F)),
-                PartPose.offsetAndRotation(5.0F,2.0F,0.0F,0.18F,0.0F,-0.12F));
+                CubeListBuilder.create().mirror().texOffs(40,16)
+                        .addBox(-1.5F,-2,-2,4,14,4),
+                PartPose.offsetAndRotation(4.5F,2.0F,0.0F,0.15F,0.0F,-0.08F));
 
         root.addOrReplaceChild("right_leg",
-                CubeListBuilder.create().texOffs(0,16).addBox(-2,0,-2,4,12,4,new CubeDeformation(-0.1F)),
-                PartPose.offset(-2.0F,12.0F,0.0F));
+                CubeListBuilder.create().texOffs(0,16)
+                        .addBox(-2,0,-2,4,12,4),
+                PartPose.offset(-1.9F,12.0F,0.0F));
+
         root.addOrReplaceChild("left_leg",
-                CubeListBuilder.create().mirror().texOffs(0,16).addBox(-2,0,-2,4,12,4,new CubeDeformation(-0.1F)),
-                PartPose.offset(2.0F,12.0F,0.0F));
+                CubeListBuilder.create().mirror().texOffs(0,16)
+                        .addBox(-2,0,-2,4,12,4),
+                PartPose.offset(1.9F,12.0F,0.0F));
 
         return LayerDefinition.create(mesh,64,64);
     }
