3e20e953cec73ae1119aad560d14ffb89be5aa88
remiusomg-ops <remiusomg@gmail.com>
2026-09-05 12:45:53 -0300

    Corregir renderer manteniendo capa visual Husk

--- src/main/java/com/renzo/caminantenocturno/client/CaminanteNocturnoRenderer.java
+++ src/main/java/com/renzo/caminantenocturno/client/CaminanteNocturnoRenderer.java
@@ -3,18 +3,18 @@ package com.renzo.caminantenocturno.client;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.renzo.caminantenocturno.CaminanteNocturnoMod;
 import com.renzo.caminantenocturno.entity.CaminanteNocturnoEntity;
-import net.minecraft.client.model.ZombieModel;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
-public class CaminanteNocturnoRenderer extends MobRenderer<CaminanteNocturnoEntity, ZombieModel<CaminanteNocturnoEntity>> {
+public class CaminanteNocturnoRenderer extends MobRenderer<CaminanteNocturnoEntity, HumanoidModel<CaminanteNocturnoEntity>> {
     private static final ResourceLocation TEXTURE =
         new ResourceLocation(CaminanteNocturnoMod.MODID, "textures/entity/caminante_nocturno.png");
 
     public CaminanteNocturnoRenderer(EntityRendererProvider.Context context) {
-        super(context, new ZombieModel<>(context.bakeLayer(ModelLayers.HUSK)), 0.5F);
+        super(context, new HumanoidModel<>(context.bakeLayer(ModelLayers.HUSK)), 0.5F);
     }
 
     @Override
