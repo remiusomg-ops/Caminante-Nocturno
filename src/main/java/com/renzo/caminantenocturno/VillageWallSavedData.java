f0d614af7ad6fceb6ed6a57dd37d9d99da26b7bb
remiusomg-ops <remiusomg@gmail.com>
2026-09-05 15:22:02 -0300

    Mejorar deduplicacion persistente de aldeas

--- src/main/java/com/renzo/caminantenocturno/VillageWallSavedData.java
+++ src/main/java/com/renzo/caminantenocturno/VillageWallSavedData.java
@@ -1,52 +1,48 @@
 package com.renzo.caminantenocturno;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.LongTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.saveddata.SavedData;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class VillageWallSavedData extends SavedData {
-    private static final String DATA_NAME = "caminantenocturno_village_walls";
+    private static final String DATA_NAME = "caminantenocturno_village_walls_v2";
     private final Set<Long> walledVillages = new HashSet<>();
 
-    public static VillageWallSavedData get(net.minecraft.server.level.ServerLevel level) {
-        return level.getDataStorage().computeIfAbsent(
-                VillageWallSavedData::load,
-                VillageWallSavedData::new,
-                DATA_NAME
-        );
+    public static VillageWallSavedData get(ServerLevel level) {
+        return level.getDataStorage().computeIfAbsent(VillageWallSavedData::load, VillageWallSavedData::new, DATA_NAME);
     }
 
-    public boolean isWalled(long key) {
-        return walledVillages.contains(key);
+    public boolean isNearWalled(BlockPos pos, int radius) {
+        long r2=(long)radius*radius;
+        for(long key:walledVillages) if(BlockPos.of(key).distSqr(pos)<=r2) return true;
+        return false;
     }
+
+    public boolean isWalled(long key) { return walledVillages.contains(key); }
 
     public void markWalled(long key) {
-        if (walledVillages.add(key)) {
-            setDirty();
-        }
+        if (walledVillages.add(key)) setDirty();
     }
 
     public static VillageWallSavedData load(CompoundTag tag) {
-        VillageWallSavedData data = new VillageWallSavedData();
-        ListTag list = tag.getList("villages", Tag.TAG_LONG);
-        for (Tag value : list) {
-            data.walledVillages.add(((LongTag) value).getAsLong());
-        }
+        VillageWallSavedData data=new VillageWallSavedData();
+        ListTag list=tag.getList("villages", Tag.TAG_LONG);
+        for(Tag value:list) data.walledVillages.add(((LongTag)value).getAsLong());
         return data;
     }
 
     @Override
     public CompoundTag save(CompoundTag tag) {
-        ListTag list = new ListTag();
-        for (long key : walledVillages) {
-            list.add(LongTag.valueOf(key));
-        }
-        tag.put("villages", list);
+        ListTag list=new ListTag();
+        for(long key:walledVillages) list.add(LongTag.valueOf(key));
+        tag.put("villages",list);
         return tag;
     }
 }
