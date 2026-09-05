0dd043dddbe8661592afd83ce9f1304844b2b445
remiusomg-ops <remiusomg@gmail.com>
2026-09-05 12:50:54 -0300

    Registrar capa del nuevo modelo

--- src/main/java/com/renzo/caminantenocturno/client/ClientEvents.java
+++ src/main/java/com/renzo/caminantenocturno/client/ClientEvents.java
@@ -4,7 +4,13 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
 @Mod.EventBusSubscriber(modid=CaminanteNocturnoMod.MODID,bus=Mod.EventBusSubscriber.Bus.MOD,value=Dist.CLIENT)
-public final class ClientEvents{
- @SubscribeEvent public static void registerRenderers(EntityRenderersEvent.RegisterRenderers e){e.registerEntityRenderer(CaminanteNocturnoMod.CAMINANTE_NOCTURNO.get(),CaminanteNocturnoRenderer::new);}
+public final class ClientEvents {
+ @SubscribeEvent public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions e){
+   e.registerLayerDefinition(CaminanteNocturnoRenderer.LAYER,CaminanteNocturnoModel::createBodyLayer);
+ }
+ @SubscribeEvent public static void registerRenderers(EntityRenderersEvent.RegisterRenderers e){
+   e.registerEntityRenderer(CaminanteNocturnoMod.CAMINANTE_NOCTURNO.get(),CaminanteNocturnoRenderer::new);
+ }
 }
