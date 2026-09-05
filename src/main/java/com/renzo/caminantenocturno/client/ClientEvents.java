d6f5734da5cb7bae9a4ab277b544eb0a43d42627
remiusomg-ops <remiusomg@gmail.com>
2026-09-05 12:12:06 -0300

    Agregar src/main/java/com/renzo/caminantenocturno/client/ClientEvents.java

---
+++ src/main/java/com/renzo/caminantenocturno/client/ClientEvents.java
@@ -0,0 +1,10 @@
+package com.renzo.caminantenocturno.client;
+import com.renzo.caminantenocturno.CaminanteNocturnoMod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+@Mod.EventBusSubscriber(modid=CaminanteNocturnoMod.MODID,bus=Mod.EventBusSubscriber.Bus.MOD,value=Dist.CLIENT)
+public final class ClientEvents{
+ @SubscribeEvent public static void registerRenderers(EntityRenderersEvent.RegisterRenderers e){e.registerEntityRenderer(CaminanteNocturnoMod.CAMINANTE_NOCTURNO.get(),CaminanteNocturnoRenderer::new);}
+}
