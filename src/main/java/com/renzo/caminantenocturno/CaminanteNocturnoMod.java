67a002483358a6f88f49a29173a22d320c4e367d
remiusomg-ops <remiusomg@gmail.com>
2026-09-05 13:07:57 -0300

    Agregar frasco explosivo

--- src/main/java/com/renzo/caminantenocturno/CaminanteNocturnoMod.java
+++ src/main/java/com/renzo/caminantenocturno/CaminanteNocturnoMod.java
@@ -1,6 +1,8 @@
 package com.renzo.caminantenocturno;
 
 import com.renzo.caminantenocturno.entity.CaminanteNocturnoEntity;
+import com.renzo.caminantenocturno.entity.FrascoExplosivoEntity;
+import com.renzo.caminantenocturno.item.FrascoExplosivoItem;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.EntityType;
@@ -23,25 +25,44 @@ public class CaminanteNocturnoMod {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
+
     public static final RegistryObject<EntityType<CaminanteNocturnoEntity>> CAMINANTE_NOCTURNO = ENTITY_TYPES.register(
         "caminante_nocturno",
         () -> EntityType.Builder.of(CaminanteNocturnoEntity::new, MobCategory.MONSTER)
             .sized(0.6F, 1.95F).clientTrackingRange(10).build("caminantenocturno:caminante_nocturno")
     );
+
+    public static final RegistryObject<EntityType<FrascoExplosivoEntity>> FRASCO_EXPLOSIVO_ENTITY = ENTITY_TYPES.register(
+        "frasco_explosivo",
+        () -> EntityType.Builder.<FrascoExplosivoEntity>of(FrascoExplosivoEntity::new, MobCategory.MISC)
+            .sized(0.25F,0.25F).clientTrackingRange(8).updateInterval(10).build("caminantenocturno:frasco_explosivo")
+    );
+
     public static final RegistryObject<SoundEvent> AULLIDO = SOUNDS.register(
         "aullido", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "aullido"))
     );
+
     public static final RegistryObject<Item> CAMINANTE_NOCTURNO_SPAWN_EGG = ITEMS.register(
         "caminante_nocturno_spawn_egg",
         () -> new ForgeSpawnEggItem(CAMINANTE_NOCTURNO, 0x201B1B, 0x793030, new Item.Properties())
     );
+
+    public static final RegistryObject<Item> FRASCO_EXPLOSIVO = ITEMS.register(
+        "frasco_explosivo",
+        () -> new FrascoExplosivoItem(new Item.Properties().stacksTo(16))
+    );
+
     public CaminanteNocturnoMod() {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
-        ENTITY_TYPES.register(modBus); ITEMS.register(modBus); SOUNDS.register(modBus);
+        ENTITY_TYPES.register(modBus);
+        ITEMS.register(modBus);
+        SOUNDS.register(modBus);
         modBus.addListener(this::addCreative);
         MinecraftForge.EVENT_BUS.register(this);
     }
+
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.SPAWN_EGGS) event.accept(CAMINANTE_NOCTURNO_SPAWN_EGG);
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) event.accept(FRASCO_EXPLOSIVO);
     }
 }
